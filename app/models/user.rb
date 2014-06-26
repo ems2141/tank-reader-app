9148079c0377ca7c8808f70030166f2a6b3b5238
Ellie Schneiders <ems2141@gmail.com>
2014-06-26 10:46:21 -0600

    user must have a unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
+
+  validates :email, {presence: true, uniqueness: true }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
   before do
     @user = User.new(
         name: 'Jay',
-        email: 'jay@text.com',
+        email: 'jay@test.com',
         password: 'password'
     )
     expect(@user).to be_valid
@@ -14,6 +14,21 @@ describe User do
   it 'must have a name' do
     @user.name = ""
     expect(@user).to_not be_valid
+  end
+
+  it 'must have a valid email' do
+    @user.email = 'test'
+    expect(@user).to_not be_valid
+  end
+
+  it 'must have a unique email' do
+    @user.save
+    user_1 = User.new(name: 'Jay',
+        email: 'jay@test.com',
+        password: 'password'
+    )
+
+    expect(user_1).to_not be_valid
   end
 
 end
