5f02ad870e1152021a3392c3752684e25fb99390
Ellie Schneiders <ems2141@gmail.com>
2014-07-10 10:40:58 -0600

    [#74050702] user can edit and update a tank

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -14,4 +14,22 @@ class TanksController < ApplicationController
       redirect_to station_path(@station)
     end
   end
+
+  def show
+    @tank = Tank.find(params[:id])
+    @station = Station.find(params[:station_id])
+  end
+
+  def edit
+    @tank = Tank.find(params[:id])
+    @station = Station.find(params[:station_id])
+  end
+
+  def update
+    @tank = Tank.find(params[:id])
+    @tank.name = params[:tank][:name]
+    @tank.save
+    @station = Station.find(params[:station_id])
+    redirect_to station_path(@station)
+  end
 end
--- spec/features/tank_spec.rb
+++ spec/features/tank_spec.rb
@@ -21,4 +21,24 @@ feature 'Tanks' do
     expect(page).to have_content 'Sinclair'
     expect(page).to have_content 'Tank 1'
   end
+
+  scenario 'user can show a tank' do
+    create_station(user_id: @user.id,
+                   name: 'Sinclair')
+
+    visit '/stations'
+    click_on 'Sinclair'
+    click_on 'Add Tank'
+
+    fill_in 'Name', with: 'Tank 1'
+    click_on 'Create Tank'
+
+    click_on 'Tank 1'
+    click_on 'Edit'
+    fill_in 'Name', with: 'A better name'
+    click_on 'Update'
+
+    expect(page).to have_content 'Sinclair'
+    expect(page).to have_content 'A better name'
+  end
 end
